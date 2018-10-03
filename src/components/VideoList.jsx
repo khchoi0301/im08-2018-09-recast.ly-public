eca2b61bc37842733c1cba6eb6b2baa0bfee0a22
Lee Hoyong <igotoweb@gmail.com>
2018-10-03 23:55:57 +0900

    fix broken propTypes

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -15,7 +15,7 @@ var VideoList = () => (
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
+  videos: PropTypes.array.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
